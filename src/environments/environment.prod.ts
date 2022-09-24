de3c642e23c8a8fd7e185099abd543bf2cc55f3a
David Gomez Muns <davidmuns@yahoo.es>
2022-09-24 14:30:43 +0200

    acces login backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   baseApi: 'https://swapi.dev/api/',
-  pagedUrl: 'https://swapi.dev/api/starships/?page='
+  pagedUrl: 'https://swapi.dev/api/starships/?page=',
+  EMAIL_PASSWORD_URL: ' https://davidmuns-login.herokuapp.com/email-password/',
+  AUTH_URL: 'https://davidmuns-login.herokuapp.com/auth/'
 };
