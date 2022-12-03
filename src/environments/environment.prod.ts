f604321cd49806039ba1b64fc938c7c37d9745d0
David Gomez Muns <davidmuns@yahoo.es>
2022-12-03 18:56:25 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseApi: 'https://swapi.dev/api/',
-  pagedUrl: 'https://swapi.dev/api/starships/?page=',
+  baseApi: 'https://swapi.py4e.com/api/',
+  pagedUrl: 'https://swapi.py4e.com/api/starships/?page=',
   EMAIL_PASSWORD_URL: ' https://blogexapp.herokuapp.com/email-password/',
   AUTH_URL: 'https://blogexapp.herokuapp.com/auth/'
 };
