3287b73468cab62cd46d976a63c653e5f8cac3c2
David Gomez Muns <davidmuns@yahoo.es>
2024-10-14 14:32:09 +0200

    new heroku url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: true,
   baseApi: 'https://swapi.py4e.com/api/',
   pagedUrl: 'https://swapi.py4e.com/api/starships/?page=',
-  EMAIL_PASSWORD_URL: ' https://blogexapp.herokuapp.com/email-password/',
-  AUTH_URL: 'https://blogexapp.herokuapp.com/auth/'
+  EMAIL_PASSWORD_URL: ' https://blogexapp-7d861950990b.herokuapp.com/email-password/',
+  AUTH_URL: 'https://blogexapp-7d861950990b.herokuapp.com/auth/'
 };
 
 
